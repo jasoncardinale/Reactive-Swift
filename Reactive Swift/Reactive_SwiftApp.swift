75f1ff6a616615ccd6085e40c9df093c6c2e9165
Jason <jasoncardinale19@gmail.com>
2022-12-12 21:32:36 -0500

    Initial Commit

---
+++ Reactive Swift/Reactive_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reactive_SwiftApp.swift
+//  Reactive Swift
+//
+//  Created by Jason Cardinale on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Reactive_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
